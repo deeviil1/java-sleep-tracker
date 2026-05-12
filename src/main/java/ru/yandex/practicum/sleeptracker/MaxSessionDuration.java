76bdf4a655f71397da8bea5ef93cd5858d2271b8
User Androsenko <urij@MacBook-Air-Urij.local>
2026-05-12 22:28:40 +0200

    1

--- src/main/java/ru/yandex/practicum/sleeptracker/MaxSessionDuration.java
+++ src/main/java/ru/yandex/practicum/sleeptracker/MaxSessionDuration.java
@@ -7,7 +7,7 @@ public class MaxSessionDuration implements SleepAnalizer {
     @Override
     public SleepAnalysisResult analizer(List<SleepingSession> session) {
         if (session.isEmpty()) {
-            return new SleepAnalysisResult("Максимальная длительность сессии", 0);
+            return new SleepAnalysisResult("Максимальная длительность сессии", 0L);
         }
         long maxMinutes = session.stream()
                 .mapToLong(sessions -> Duration.between(sessions.getStartSession(), sessions.getEndSession()).toMinutes())
